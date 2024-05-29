c41578a07ed9e9d9a05b94225444068fe5f3f521
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-05-29 21:28:23 +0900

    try

--- python/anything.py
+++ python/anything.py
@@ -1,1 +1,14 @@
-print([a*2 for a in range(5)])
+class asdf():
+    def __init__(self):
+        self.jan = 0
+    def plus(self, ina):
+        self.ina = ina
+        self.jan+=self.jan+ina
+        print(self.jan)
+    def minus(self,aut):
+        self.aut = aut
+        print([self.jan-aut if self.jan>=aut else "잔액이 부족합니다."][0])
+asd = asdf()
+asd.plus(10)
+asd.minus(5)
+asd.minus(55)
